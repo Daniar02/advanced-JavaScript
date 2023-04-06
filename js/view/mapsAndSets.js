9dfd07454d24a14aeedc85281356e7bc7f80f458
Daniar02 <abykeev02@icloud.com>
2023-04-06 17:06:01 +0600

    mapsAndSets: Sets

--- js/view/mapsAndSets.js
+++ js/view/mapsAndSets.js
@@ -1,5 +1,25 @@
 function init() {
+    /* #################### Sets ####################*/
+    const flights = ["Russia", "USD", "London"];
 
+    const setFlights = new Set(flights);
+    console.log(setFlights);
+    console.log(setFlights.size);
+    console.log(setFlights.has("USD"));
+    // setFlights.clear();
+    setFlights.add("Paris");
+    setFlights.delete("USD");
+    console.log(setFlights);
+
+    for (const flight of setFlights) {
+        console.log(flight)
+    }
+
+    console.log([...setFlights]);
+
+    const setObj = new Set([{a: 2}, {b: 2}, {b: 2}]);
+    console.log(setObj);
+    console.log(new Set("abc"));
 }
 
 export default init

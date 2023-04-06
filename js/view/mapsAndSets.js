4187c010fa7647553d39bf595681baf257f304fb
Daniar02 <abykeev02@icloud.com>
2023-04-06 21:16:44 +0600

    mapsAndSets: Итерация по Map

--- js/view/mapsAndSets.js
+++ js/view/mapsAndSets.js
@@ -67,6 +67,21 @@ function init() {
     // const weatherObject2 = new Map(Object.entries(weatherObject));
     // console.log(weatherObject2);
 
+    /* #################### Итерация по Map ####################*/
+    // const weatherMap = new Map([
+    //     ["London", "10"],
+    //     ["Moscow", "7"],
+    //     ["Paris", "14"],
+    // ]);
+    // for (const [key, value] of weatherMap) {
+    //     console.log(key);
+    //     console.log(value);
+    // }
+    //
+    // console.log([...weatherMap]);
+    // console.log([...weatherMap.keys()]);
+    // console.log([...weatherMap.values()]);
+
 }
 
 export default init

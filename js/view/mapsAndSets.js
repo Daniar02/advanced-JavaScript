d2560b63b7e622a1bc01f8ac3ff0a0a8518dbef3
Daniar02 <abykeev02@icloud.com>
2023-04-07 04:59:26 +0600

    mapsAndSets: WeakMap, WeakSet

--- js/view/mapsAndSets.js
+++ js/view/mapsAndSets.js
@@ -92,6 +92,44 @@ function init() {
     // weatherMap = new Map([...weatherMap].map(el => el.reverse()))
     // console.log(weatherMap);
 
+    /* #################### WeakMap ####################*/
+    // let a = { a: 1 };
+    // let b = { b: 2 };
+    // const map = new WeakMap();
+    // map.set(a, "testA");
+    // map.set(b, "testB");
+    // console.log(map.get(a));
+    // console.log(map.has(a));
+    // console.log(map);
+    // a = null;
+    //
+    // setTimeout(()=> {
+    //     console.log(map);
+    // }, 1000)
+    //
+    // let cache = new WeakMap();
+    //
+    // function getValue(obj) {
+    //     if (!cache.has(obj)) {
+    //         const res = 1;
+    //         cache.set(obj, res)
+    //     }
+    //     return cache.get(obj)
+    // }
+    // const res = getValue(b);
+    // console.log(res);
+    // const res2 = getValue(b);
+    // console.log(res2);
+
+    /* #################### WeakSet ####################*/
+    // let a = { a: 1 };
+    // let b = { b: 2 };
+    // const set = new WeakSet([a, b]);
+    // a = null;
+    // setTimeout(() => {
+    //     console.log(set);
+    // }, 1000);
+
 
 }
 

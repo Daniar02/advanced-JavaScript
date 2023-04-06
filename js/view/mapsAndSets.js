3b3a1614c76a3c65a3c6d95c453d6678ecb3ee25
Daniar02 <abykeev02@icloud.com>
2023-04-06 19:24:20 +0600

    mapsAndSets: Основы работы с Map

--- js/view/mapsAndSets.js
+++ js/view/mapsAndSets.js
@@ -1,25 +1,55 @@
 function init() {
     /* #################### Sets ####################*/
-    const flights = ["Russia", "USD", "London"];
+    // const flights = ["Russia", "USD", "London"];
+    //
+    // const setFlights = new Set(flights);
+    // console.log(setFlights);
+    // console.log(setFlights.size);
+    // console.log(setFlights.has("USD"));
+    // // setFlights.clear();
+    // setFlights.add("Paris");
+    // setFlights.delete("USD");
+    // console.log(setFlights);
+    //
+    // for (const flight of setFlights) {
+    //     console.log(flight)
+    // }
+    //
+    // console.log([...setFlights]);
+    //
+    // const setObj = new Set([{a: 2}, {b: 2}, {b: 2}]);
+    // console.log(setObj);
+    // console.log(new Set("abc"));
 
-    const setFlights = new Set(flights);
-    console.log(setFlights);
-    console.log(setFlights.size);
-    console.log(setFlights.has("USD"));
-    // setFlights.clear();
-    setFlights.add("Paris");
-    setFlights.delete("USD");
-    console.log(setFlights);
+    /* #################### Основы работы с Map ####################*/
+    // const weatherMap = new Map();
+    // weatherMap.set('London', 10)
+    //           .set('Moscow', 7);
+    //
+    // console.log(weatherMap.get("London"));
+    // console.log(weatherMap.get("not found"));
+    //
+    // console.log(weatherMap.has("London"));
+    // console.log(weatherMap.has("not found"));
+    //
+    // console.log(weatherMap.delete("London"));
+    // console.log(weatherMap.delete("not found"));
+    // weatherMap.clear();
+    //
+    // const array = [1, 2, 3]
+    // weatherMap
+    //     .set(1, 5)
+    //     .set(true, 'yes')
+    //     .set(false, 'no')
+    //     .set(array, 'array')
+    //     .set({ a: 1 }, {b: 2});
+    //
+    // console.log(weatherMap);
+    // console.log(weatherMap.size);
+    //
+    // console.log(weatherMap.get(array));
+    // console.log(weatherMap.get({ a: 1 }));
 
-    for (const flight of setFlights) {
-        console.log(flight)
-    }
-
-    console.log([...setFlights]);
-
-    const setObj = new Set([{a: 2}, {b: 2}, {b: 2}]);
-    console.log(setObj);
-    console.log(new Set("abc"));
 }
 
 export default init

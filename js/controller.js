4e40dee626a21399b4a073b03d61e6fb8cfa824a
Daniar02 <abykeev02@icloud.com>
2023-04-07 05:41:48 +0600

    calculationsAndMath: Math, Округление

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,8 @@
 import mapsAndSets from "./view/mapsAndSets.js";
+import calculationsAndMath from "./view/calculationsAndMath.js";
 
 window.onload = function () {
 
     mapsAndSets();
+    calculationsAndMath();
 }
---
+++ js/view/calculationsAndMath.js
@@ -0,0 +1,17 @@
+function init() {
+    /* #################### Math, Округление ####################*/
+    // const arr = [1, -2, 10, 0, 19];
+    // console.log(Math.max(...arr));
+    // console.log(Math.round(1.499999));
+    // console.log(Math.ceil(1.1));
+    // console.log(Math.ceil(0.1));
+    // console.log(Math.trunc(1.1111));
+    // console.log(Number((1.499999).toFixed(1)));
+    //
+    // function random(min, max) {
+    //     return Math.floor(Math.random() * (max - min + 1) + min)
+    // }
+    // console.log(random(1, 20));
+}
+
+export default init;

e3bf6b4b26115512576651be0128c957994d1b16
Daniar02 <abykeev02@icloud.com>
2023-04-06 21:03:49 +0600

    mapsAndSets: Быстрое создание Map

--- js/view/mapsAndSets.js
+++ js/view/mapsAndSets.js
@@ -50,6 +50,23 @@ function init() {
     // console.log(weatherMap.get(array));
     // console.log(weatherMap.get({ a: 1 }));
 
+    /* #################### Быстрое создание Map ####################*/
+
+    // const weatherMap = new Map([
+    //     ["London", "10"],
+    //     ["Moscow", "7"],
+    // ]);
+    // console.log(weatherMap);
+    //
+    // const weatherObject = {
+    //     london: "10",
+    //     moscow: "7",
+    //     paris: "14"
+    // }
+    // console.log(Object.entries(weatherObject));
+    // const weatherObject2 = new Map(Object.entries(weatherObject));
+    // console.log(weatherObject2);
+
 }
 
 export default init

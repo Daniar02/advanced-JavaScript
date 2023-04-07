14f8f3680006bf19491680d32d827f2f5f9f04aa
Daniar02 <abykeev02@icloud.com>
2023-04-07 06:20:09 +0600

    calculationsAndMath: Интернационализация чисел

--- js/view/calculationsAndMath.js
+++ js/view/calculationsAndMath.js
@@ -12,6 +12,32 @@ function init() {
     //     return Math.floor(Math.random() * (max - min + 1) + min)
     // }
     // console.log(random(1, 20));
+
+    /* #################### Интернационализация чисел ####################*/
+    // const options1 = {
+    //     style: "currency",
+    //     currency: 'RUB'
+    // }
+    // const options2 = {
+    //     style: "currency",
+    //     currency: 'USD'
+    // }
+    // const options3 = {
+    //     style: "decimal"
+    // }
+    // const options4 = {
+    //     style: "percent",
+    // }
+    // const options5 = {
+    //     style: "unit",
+    //     unit: "celsius"
+    // }
+    // console.log(new Intl.NumberFormat('ru-RU', options1).format(23000));
+    // console.log(new Intl.NumberFormat('en-US', options2).format(23000));
+    // console.log(new Intl.NumberFormat('ru-RU', options3).format(23000));
+    // console.log(new Intl.NumberFormat('ru-RU', options4).format(0.1));
+    // console.log(new Intl.NumberFormat('ru-RU', options5).format(0.1));
+
 }
 
 export default init;

96af3e1db5cdb26943dcd8baf947087dddea9254
Daniar02 <abykeev02@icloud.com>
2023-04-07 16:28:30 +0600

    Timers: Работа с интервалами, Упражнение - Таймер пиццы

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,12 @@
 import mapsAndSets from "./view/mapsAndSets.js";
 import calculationsAndMath from "./view/calculationsAndMath.js";
 import datesAndTimes from "./view/datesAndTimes.js";
+import timers from "./view/timers.js";
 
 window.onload = function () {
 
     mapsAndSets();
     calculationsAndMath();
     datesAndTimes();
+    timers();
 }

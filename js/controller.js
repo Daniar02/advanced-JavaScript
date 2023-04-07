3de2b74a386861af45cfed297056b99b474afbab
Daniar02 <abykeev02@icloud.com>
2023-04-07 12:54:15 +0600

    calculationsAndMath: Создание дат

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,10 @@
 import mapsAndSets from "./view/mapsAndSets.js";
 import calculationsAndMath from "./view/calculationsAndMath.js";
+import datesAndTimes from "./view/datesAndTimes.js";
 
 window.onload = function () {
 
     mapsAndSets();
     calculationsAndMath();
+    datesAndTimes();
 }
---
+++ js/view/datesAndTimes.js
@@ -0,0 +1,31 @@
+function init() {
+    /* #################### Создание дат #################### */
+    // const now = new Date();
+    // console.log(now);
+    //
+    // console.log(new Date("01-02-2023"));
+    // console.log(new Date("01/02/2023"));
+    // console.log(new Date("2023/01/02"));
+    // console.log(new Date("10 Jan 2023"));
+    //
+    // console.log(new Date(2023, 11, 31, 12, 0, 10));
+    // console.log(new Date(2023, 12, 10 + 100));
+    // console.log(new Date(0));
+    // console.log(new Date(1 * 24 * 60 * 60 * 1000));
+    // console.log(Date.now());
+    // console.log(new Date(Date.now()));
+    //
+    // console.log(now.getFullYear());
+    // console.log(now.getMonth());
+    // console.log(now.getDate());
+    // console.log(now.getDay());
+    // console.log(now.getHours());
+    // console.log(now.getMinutes());
+    // console.log(now.getSeconds());
+    // console.log(now.getTime());
+    //
+    // console.log(new Date(now.setFullYear(2030)));
+
+}
+
+export default init

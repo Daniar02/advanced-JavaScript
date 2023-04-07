a080b0dec3b3ed261e9ae3750637f4103ba89743
Daniar02 <abykeev02@icloud.com>
2023-04-07 17:31:06 +0600

    ООП: Что же такое ООП?

--- js/controller.js
+++ js/controller.js
@@ -2,6 +2,7 @@ import mapsAndSets from "./view/mapsAndSets.js";
 import calculationsAndMath from "./view/calculationsAndMath.js";
 import datesAndTimes from "./view/datesAndTimes.js";
 import timers from "./view/timers.js";
+import oop from "./view/oop.js";
 
 window.onload = function () {
 
@@ -9,4 +10,5 @@ window.onload = function () {
     calculationsAndMath();
     datesAndTimes();
     timers();
+    oop();
 }

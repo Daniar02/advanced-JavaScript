903693bd3121d9269760625c4028cbb062aac084
Daniar02 <abykeev02@icloud.com>
2023-04-07 13:26:36 +0600

    datesAndTimes: Операции с датами, Сравнение дат

--- js/view/datesAndTimes.js
+++ js/view/datesAndTimes.js
@@ -26,6 +26,32 @@ function init() {
     //
     // console.log(new Date(now.setFullYear(2030)));
 
+    /* #################### Операции с датами #################### */
+    // const date1 = new Date(2024, 10, 15);
+    // const date2 = new Date(2024, 11, 15);
+    //
+    // console.log(Number(date1));
+    // console.log(Number(date2));
+    // console.log(Number(date1 - date2));
+    //
+    // function getDaysBetweenDates(dateFirst, dateSecond) {
+    //     return (date2 - date1) / (1000 * 60 * 60 * 24);
+    // }
+    // console.log(getDaysBetweenDates(date1, date2))
+
+    /* #################### Сравнение дат #################### */
+    // const first = new Date(2024, 10, 4);
+    // const second = new Date(2024, 10, 4);
+    //
+    // console.log(first < second);
+    // console.log(first == second);
+    // console.log(first === second);
+    //
+    // console.log(first.getTime() == second.getTime());
+    // console.log(first.getTime() === second.getTime());
+    //
+    // console.log(Number(first) === Number(second));
+    // console.log(+first === +second);
 }
 
 export default init

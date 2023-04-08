5587865ad98c718e71b235bef5c129d48402513b
Daniar02 <abykeev02@icloud.com>
2023-04-08 17:43:44 +0600

    Solid: Принцип открытости и закрытости.

--- js/view/solid.js
+++ js/view/solid.js
@@ -23,5 +23,35 @@ function init() {
     //     }
     // }
 
+    /* #################### Принцип открытости и закрытости. #################### */
+    // class Treasure {
+    //     value = 0;
+    // }
+    //
+    // class Coin extends  Treasure {
+    //     value = 1;
+    // }
+    //
+    // class Crystal extends Treasure {
+    //     value = 10;
+    // }
+    //
+    // class Brilliant extends Treasure {
+    //     value = 20;
+    // }
+    //
+    // class Inventory {
+    //     #score;
+    //     pick(treasure) {
+    //         this.#score += treasure.value;
+    //         // if (treasure instanceof Coin) {
+    //         //     this.#score += 1;
+    //         // }
+    //         // if (treasure instanceof Crystal) {
+    //         //     this.#score += 10;
+    //         // }
+    //     }
+    // }
+
 }
 export default init;

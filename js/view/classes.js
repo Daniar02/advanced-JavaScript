11fbb6878ddc2260a6f5e6db5c6e34680de1e080
Daniar02 <abykeev02@icloud.com>
2023-04-08 11:16:25 +0600

    Class: Упражнение - класс пользователя

--- js/view/classes.js
+++ js/view/classes.js
@@ -114,6 +114,44 @@ function init() {
     // const car = new Car();
     // car.test();
 
+    /* #################### Упражнение - класс пользователя. #################### */
+    // class User {
+    //     #login;
+    //     #_password;
+    //     constructor(login, password) {
+    //         this.#login = login;
+    //         this.#password = password;
+    //     }
+    //
+    //     set #password(pass) {
+    //         this.#_password = pass.split('').reverse().join('');
+    //     }
+    //
+    //     get #password() {
+    //         return this.#_password.split('').reverse().join('');
+    //     }
+    //
+    //     get login() {
+    //         return this.#login;
+    //     }
+    //
+    //     checkPassword(pass) {
+    //         return this.#password === pass;
+    //     }
+    //
+    //     changePassword(oldPass, newPas) {
+    //         if (!this.checkPassword(oldPass)){
+    //             return false;
+    //         }
+    //         this.#password = newPas;
+    //         return true;
+    //     }
+    // }
+    // const user = new User('a@a.kg', '123');
+    // console.log(user);
+    // console.log(user.checkPassword('123'));
+    // console.log(user.changePassword('123', '234'));
+    // console.log(user.checkPassword('234'));
 
 }
 

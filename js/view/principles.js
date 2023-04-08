9a8c79d3e15672c18046a397cd0669ca1afcf1f0
Daniar02 <abykeev02@icloud.com>
2023-04-08 16:14:31 +0600

    Principles: Паттерн Builder и chaining

--- js/view/principles.js
+++ js/view/principles.js
@@ -197,5 +197,45 @@ function init() {
             ● Полиморфизм подтипов
     */
 
+    /* #################### Паттерн Builder и chaining #################### */
+    // class Wallet {
+    //     balance = 0;
+    //
+    //     add(sum) {
+    //         this.balance += sum;
+    //         return this;
+    //     }
+    //
+    //     remove(sum) {
+    //         this.balance -= sum;
+    //         return this;
+    //     }
+    // }
+    //
+    // const wallet = new Wallet();
+    // const res = wallet.add(100).remove(10);
+    // console.log(res)
+    //
+    // class Builder {
+    //     house = {};
+    //
+    //     addRoof() {
+    //         this.house.roof = 'Roof';
+    //         return this;
+    //     }
+    //
+    //     addFloor() {
+    //         this.house.floor = 'Floor';
+    //         return this;
+    //     }
+    //
+    //     execute() {
+    //         return this.house;
+    //     }
+    // }
+    //
+    // const res2 = new Builder().addRoof().addFloor().execute();
+    // console.log(res2);
+
 }
 export default init;

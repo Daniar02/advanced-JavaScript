44691f2daca0caa0f77d109376a39866c78f114b
Daniar02 <abykeev02@icloud.com>
2023-04-08 14:07:56 +0600

    Principles: Override методов

--- js/view/principles.js
+++ js/view/principles.js
@@ -98,6 +98,38 @@ function init() {
     // book.log();
     // book.buy();
 
+    /* #################### Override методов. #################### */
+    // class Book {
+    //     constructor(title, author) {
+    //         this.title = title;
+    //         this.author = author;
+    //     }
+    //
+    //     info() {
+    //         console.log(`${this.title} - ${this.author}`);
+    //     }
+    // }
+    //
+    // const book = new Book('lord Of The Rings', 'Tolkien');
+    // book.info();
+    //
+    // class EBook extends Book {
+    //     constructor(title, author, pages) {
+    //         super(title, author);
+    //         this.pages = pages;
+    //     }
+    //
+    //     info() {
+    //         console.log(`${this.title} - ${this.author} - ${this.pages}`);
+    //     }
+    //
+    // }
+    //
+    // const book2 = new EBook('lord Of The Rings', 'Tolkien', 100);
+    // book2.info();
+
+
+
 
 }
 export default init;

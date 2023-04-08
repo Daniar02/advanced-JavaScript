eab75470b292d84562f5b914dddf0344243a062a
Daniar02 <abykeev02@icloud.com>
2023-04-08 14:55:45 +0600

    Principles: Упражнение - Удар по орку

--- js/view/principles.js
+++ js/view/principles.js
@@ -128,8 +128,54 @@ function init() {
     // const book2 = new EBook('lord Of The Rings', 'Tolkien', 100);
     // book2.info();
 
-
-
+    /* #################### Упражнение - Удар по орку #################### */
+    /*
+        Сделать класс врага со здоровьем и методом получения урона.
+        Сделать класс меча, которые имеет силу и методу нанесения урона.
+        Сделать класс орка, который в 50% случаев не получает урон.
+    */
+    // class Enemy {
+    //     health
+    //     constructor(health) {
+    //         this.health = health;
+    //     }
+    //
+    //     receiveDamage(damage) {
+    //         this.health = this.health - damage;
+    //         console.log(this.health);
+    //     }
+    // }
+    //
+    // class Sword {
+    //     #damage
+    //     constructor(damage) {
+    //         this.#damage = damage;
+    //     }
+    //
+    //     strike(enemy) {
+    //         enemy.receiveDamage(this.#damage)
+    //     }
+    // }
+    //
+    // class Org extends Enemy {
+    //     constructor(health) {
+    //         super(health);
+    //     }
+    //
+    //     receiveDamage(damage) {
+    //         if (Math.random() > 0.5) {
+    //             this.health = this.health - damage;
+    //         }
+    //         console.log(this.health);
+    //     }
+    //
+    // }
+    //
+    // const enemy = new Enemy(10);
+    // const org = new Org(10);
+    // const sword = new Sword(3);
+    // sword.strike(enemy);
+    // sword.strike(org);
 
 }
 export default init;

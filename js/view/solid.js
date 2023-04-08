9bbc5a99b19fd8ece1122674c76ea80ee512ec2b
Daniar02 <abykeev02@icloud.com>
2023-04-08 17:58:35 +0600

    Solid: Принцип подстановки Барбары Лисков.

--- js/view/solid.js
+++ js/view/solid.js
@@ -53,5 +53,27 @@ function init() {
     //     }
     // }
 
+    /* #################### Принцип подстановки Барбары Лисков. #################### */
+    // class User {
+    //     #role = 'user';
+    //     getRole() {
+    //         return this.#role;
+    //     }
+    // }
+    //
+    // class Admin extends User {
+    //     role = ['user', 'admin'];
+    //
+    //     getRole() {
+    //         return this.role.join(', ');
+    //     }
+    // }
+    //
+    // function logRole(user) {
+    //     console.log(`Role: ${user.getRole().toUpperCase()}`)
+    // }
+    // logRole(new User());
+    // logRole(new Admin());
+
 }
 export default init;

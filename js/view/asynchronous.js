4b1a243412009ef16fe3e708e3e1d16a2903681c
Daniar02 <abykeev02@icloud.com>
2023-04-09 15:31:20 +0600

    Asynchronous: Упражнение - Расчёт цены.

--- js/view/asynchronous.js
+++ js/view/asynchronous.js
@@ -33,5 +33,21 @@ function init() {
     // req(2);
     // req(3);
 
+    /* #################### Упражнение - Расчёт цены. #################### */
+    /*
+        Получить среднюю цены 30 товаров из АРІ
+    */
+    // const request = new XMLHttpRequest();
+    // request.open("GET", "https://dummyjson.com/products");
+    // request.send();
+    //
+    // request.addEventListener('load', function () {
+    //     const { products } = JSON.parse(this.responseText);
+    //     console.log(products);
+    //     const sum = products.reduce((acc, p) => acc += p.price, 0);
+    //     console.log(sum / products.length);
+    // })
+
+
 }
 export default init

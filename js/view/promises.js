15b7129d1f7b4dcf1e0fa4b38a58cf2fa9c07e26
Daniar02 <abykeev02@icloud.com>
2023-04-09 21:53:05 +0600

    Promises: Упражнение - Select выбора категорий.

--- js/view/promises.js
+++ js/view/promises.js
@@ -50,7 +50,25 @@ function init() {
     //         console.log('Finally');
     //     });
 
-
+    /* #################### Упражнение - Select выбора категорий. #################### */
+    /*
+        Сделать запрос на https: 'https://dummyjson.com/products/categories',
+        получить список категорий и отобразить <select> выбора категорий.
+    */
+    // function createSelect(array) {
+    //     const el = document.querySelector('body');
+    //     el.innerHTML = `<div class="filter"><select>
+    //         ${array.map(arrEl => `<option value="${arrEl}">${arrEl}</option>`)}
+    //     </select></div>`
+    // }
+    //
+    // function getCategories() {
+    //     fetch('https://dummyjson.com/products/categories')
+    //         .then(response => response.json())
+    //         .then(data => createSelect(data))
+    //         .catch(error => console.error(`Ошибка: ${error}`))
+    // }
+    // getCategories();
 
 }
 export default init

3e4fd6664da738aa46e5b6f33376f063afc8494c
Daniar02 <abykeev02@icloud.com>
2023-04-10 12:53:34 +0600

    ModernAsynchronousJavaScript: Упражнение - Мой город.

--- js/view/modernAsynchronousJavaScript.js
+++ js/view/modernAsynchronousJavaScript.js
@@ -32,5 +32,45 @@ function init() {
     // getProducts();
     // console.log('End');
 
+    /* #################### Упражнение - Мой город. #################### */
+    /*
+        Получить геолокация пользователя через
+        Geolocation.getCurrentPosition() (WEB API)
+        и по координатам определить город, отправив запрос:
+        https://api.bigdatacloud.net/data/reverse-geocode-client?
+        latitude=00&longitude=00
+    */
+    // function getMyCoordinates() {
+    //     return new Promise((resolve, reject) => {
+    //         navigator.geolocation.getCurrentPosition(
+    //             ({coords}) => {
+    //                 resolve({
+    //                     latitude: coords.latitude,
+    //                     longitude: coords.longitude,
+    //                 });
+    //             },
+    //             (err => {
+    //                 reject(err);
+    //             })
+    //         )
+    //     })
+    // }
+    // async function getMyCity() {
+    //     try {
+    //         const { latitude, longitude } = await getMyCoordinates();
+    //         const response = await  fetch(`
+    //             https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}
+    //             `);
+    //         if (!response.ok) {
+    //             throw new Error(response.status);
+    //         };
+    //         const data = await response.json();
+    //         console.log(data)
+    //     } catch (e) {
+    //         console.error(e)
+    //     }
+    // }
+    //
+    // getMyCity();
 }
 export  default init;

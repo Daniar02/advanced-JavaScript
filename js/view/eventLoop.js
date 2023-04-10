ad759019471ccf963c8c9556b489426aac370a44
Daniar02 <abykeev02@icloud.com>
2023-04-10 11:22:30 +0600

    EventLoop: Упражнение - создание fetch.

--- js/view/eventLoop.js
+++ js/view/eventLoop.js
@@ -58,7 +58,37 @@ function init() {
     // Promise.resolve('Success').then(data => console.log(data));
     // Promise.reject(new Error('Error')).catch(error => console.error(error));
 
+    /* #################### Упражнение - создание fetch. #################### */
+    /*
+        Сделать функцию myFetch, которая выполняет внутри
+        XMLHttpRequest
+    */
+    // function myFetch(url) {
+    //     return new Promise((resolve, reject) => {
+    //         const request = new XMLHttpRequest();
+    //         request.open('GET', url);
+    //         request.send();
+    //
+    //         request.addEventListener('load', function () {
+    //             if (this.status > 400) {
+    //                 reject(new Error(this.status));
+    //             }
+    //             resolve(this.responseText);
+    //         });
+    //
+    //         request.addEventListener('error', function () {
+    //             reject(new Error(this.status));
+    //         });
+    //
+    //         request.addEventListener('timeout', function () {
+    //             reject(new Error('Timeout'));
+    //         })
+    //     })
+    // }
 
+    // myFetch('https://dummyjson.com/productss')
+    //     .then(data => console.log(data))
+    //     .catch(err => console.error(err))
 
 
 

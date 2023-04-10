c6d324ddf2fe27b218a11700ac8471936dd8d7f3
Daniar02 <abykeev02@icloud.com>
2023-04-10 13:31:59 +0600

    ModernAsynchronousJavaScript: Последовательность выполнения.

--- js/view/modernAsynchronousJavaScript.js
+++ js/view/modernAsynchronousJavaScript.js
@@ -90,5 +90,32 @@ function init() {
     // }
     //
     // asyncArrow();
+
+    /* #################### Последовательность выполнения. #################### */
+    // const asyncArrow = async () => {
+    //     try {
+    //         const response = await fetch('https://dummyjson.com/products');
+    //         const data = await response.json();
+    //         return data;
+    //     } catch (e) {
+    //         console.error(e);
+    //         throw e;
+    //     }
+    // }
+    // // console.log(1);
+    // // asyncArrow()
+    // //     .then(data => {
+    // //         console.log(data);
+    // //     })
+    // //     .catch(e => console.error(e))
+    // // console.log(2);
+    //
+    // ( async () => {
+    //     console.log(1);
+    //     const res = await asyncArrow();
+    //     console.log(res);
+    //     console.log(2);
+    // })()
+
 }
 export  default init;

3b6ba41e804599182271db2075ef0c1f4baa6b14
Daniar02 <abykeev02@icloud.com>
2023-04-11 23:39:56 +0600

    DOMAndEvents: Перемещение по DOM.

--- js/view/domAndEvents.js
+++ js/view/domAndEvents.js
@@ -75,6 +75,25 @@ function init() {
     //     console.log(`Deleted ${i}`);
     // });
 
+    /* #################### Перемещение по DOM. #################### */
+    // const wrapper = document.querySelector('.wrapper');
+    // const inner = document.querySelector('.inner');
+    // const button = document.querySelector('.button');
+
+    // console.log(inner.childNodes);
+    // console.log(inner.children);
+
+    // console.log(inner.parentElement);
+    // console.log(inner.parentNode);
+
+    // console.log(button.closest('.wrapper'));
+
+    // console.log(button.previousElementSibling);
+    // console.log(button.previousSibling);
+    // console.log(button.nextElementSibling);
+    // console.log(button.nextSibling);
+    //
+    // console.log(button.parentElement.children);
 
 }
 

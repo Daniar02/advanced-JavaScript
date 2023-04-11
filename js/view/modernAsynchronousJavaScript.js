ccc0ee05586a0af85b401061df09a35003b6fee2
Daniar02 <abykeev02@icloud.com>
2023-04-11 12:04:35 +0600

    ModernAsynchronousJavaScript: Все возможности fetch.

--- js/view/modernAsynchronousJavaScript.js
+++ js/view/modernAsynchronousJavaScript.js
@@ -175,6 +175,23 @@ function init() {
     // }
     // main();
 
+    /* #################### Все возможности fetch. #################### */
+    // async function main() {
+    //     const res = await fetch('https://dummyjson.com/auth/login', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             username: 'kminchelle',
+    //             password: '0lelplR'
+    //
+    //         })
+    //     });
+    //     const data = await res.json();
+    //     console.log(data);
+    // }
+    // main();
 
 }
 export  default init;

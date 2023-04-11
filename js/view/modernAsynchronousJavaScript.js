295699627dc0de55b110ab3cd999672e93b44464
Daniar02 <abykeev02@icloud.com>
2023-04-11 11:50:48 +0600

    ModernAsynchronousJavaScript: Другие комбинации Promise.

--- js/view/modernAsynchronousJavaScript.js
+++ js/view/modernAsynchronousJavaScript.js
@@ -145,5 +145,36 @@ function init() {
     // }
     // main();
 
+    /* #################### Другие комбинации Promise. #################### */
+    // async function getProduct(id) {
+    //     const response = await fetch('https://dummyjson.com/products/' + id);
+    //     return response.json();
+    // }
+    //
+    // async function getProductError(id) {
+    //     const response = await fetch('https://dummyjsons.com/products/' + id);
+    //     return response.json();
+    // }
+    //
+    // async function main() {
+    //     const res1 = await Promise.all([
+    //         getProduct(1),
+    //         getProduct(2)
+    //     ])
+    //     console.log(res1);
+    //     const res2 = await Promise.allSettled([
+    //         getProduct(1),
+    //         getProductError(2)
+    //     ])
+    //     console.log(res2);
+    //     const res3 = await Promise.race([
+    //         getProduct(1),
+    //         getProduct(2)
+    //     ])
+    //     console.log(res3);
+    // }
+    // main();
+
+
 }
 export  default init;

a5afff728a4df11ceee8d972e5aa58033d045184
Daniar02 <abykeev02@icloud.com>
2023-04-13 05:48:28 +0600

    Modularity: Модули commonjs, Await на верхнем уровне.

---
+++ js/view/modularity.js
@@ -0,0 +1,17 @@
+'use strict'
+
+    /* #################### Модули commonjs. #################### */
+    //  export function add(f, s) {
+    //     return f + s ;
+    // }
+    //  export function sub(f, s) {
+    //     return f + s ;
+    // }
+
+    /* #################### Await на верхнем уровне. #################### */
+    // async function getProducts() {
+    //     const res = await fetch('https://dummyjson.com/products');
+    //     return res.json();
+    // }
+    //
+    // export const res = await getProducts();
